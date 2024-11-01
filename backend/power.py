a220f11b13569e7a9cf14502e581fd1f8a727e82
zakgilbert <gilbertz@hawaii.edu>
2024-11-01 11:39:08 -1000

    Touch swellpower.py

---
+++ backend/power.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+from BuoyReading import *
+from util import *
+import datetime
+
+def getSwellPower(id):
+    url = f'https://www.ndbc.noaa.gov/data/5day2/{id}_5day.spectral'
+
+    response = requests.get(url)
+    print(response.text)
+
+    return ""
--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from report import *
+from power import *
 
 app = Flask(__name__)
 
 @app.route('/report/<string:id>')
 def report(id):
     return getReport(id)
+
+@app.route('/power/<string:id>')
+def power(id):
+    return getSwellPower(id)
 
 if __name__ == "__main__":
     app.run(debug=True)
