c81bc2851d5c5112eb5a9c3e05ca332af3cb2da0
zakgilbert <gilbertz@hawaii.edu>
2024-11-01 07:40:48 -1000

    Update README.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # report_app
 A surf report web application
+
+## Running Project
+
+With two terminals
+- cd backend
+- python3 server.py
+
+- cd client
+- npm run start
---
+++ backend/constants.py
@@ -0,0 +1,13 @@
+toolTips = [
+    "Curent Time",
+    "Wave Height",
+    "Swell Height",
+    "Swell Period",
+    "Wind Wave Height",
+    "Wind Wave Period",
+    "Swell Direction",
+    "Wind Wave Direction",
+    "Wave Steepness",
+    "Average Wave Period",
+    "Mean Wave Direction"
+]
